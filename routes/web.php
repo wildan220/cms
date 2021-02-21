13d12ca9954ca81d96234d2bee4fcfc8c6e002f1
wildan220 <wildanachmad73@gmail.com>
2021-02-21 21:03:59 +0700

    praktikum pertama

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function ($id) {
+    echo "selamat datang";
+});
+Route::get('/about', function ($id) {
+    echo "1941720136-wildan achmad fachrurrozi";
+});
+Route::get('/', function ($id) {
+    echo "halaman artikel dengan ID {$id}";
 });
